135d83c01e22a7f897be5a225507c34117852a47
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-09-27 17:35:37 +0900

    フォーム送信ボタンのアニメーション実装

--- html-kaguya/scripts/script.js
+++ html-kaguya/scripts/script.js
@@ -1,8 +1,13 @@
+//初期化処理
 // ページ読み込みのフェードイン・フェードアウト
 // 各ページが読み込まれた時にはfadeoutクラスを削除
-//フェードイン動作はjQueryで実装
+// フェードイン動作はjQueryで実装
+// スマホサイズ時の送信ボタンの内容を変更
 $(window).on("load", () => {
   $("body").removeClass("fadeout").hide().fadeIn(100);
+  if (window.matchMedia("(max-width: 375px)").matches) {
+    $(".submit").attr("value", "Send");
+  }
 });
 //フェードアウト動作をCSSのtransitionを用いて実装
 $(() => {
@@ -22,14 +27,7 @@ $(() => {
   });
 });
 
-$(() => {
-  $(".nav-toggle").on("click", () => {
-    $(".nav-toggle, .header-nav").toggleClass("show");
-  });
-});
-
 // const stock_slideToggle = $(".news1::before,.news2::before,.news3::before,");
-// const stock_fadeToggle = $(".news1 p,.news2 p,.news3 p");
 // //pickup-news(入荷情報画像)スライドアニメーション
 // $(".pickup-news li").on(
 //   {
@@ -46,16 +44,41 @@ $(() => {
 //   }
 // );
 
-// ハンバーガーメニューのオンオフをactiveクラスの有無で判断する
-$(function () {
-  $(".navToggle").on("click", (e) => {
-    const target = $(e.currentTarget);
-    target.toggleClass("active");
+//
 
-    if (target.hasClass("active")) {
-      $(".header-container nav").addClass("active");
-    } else {
-      $(".header-container nav").removeClass("active");
-    }
-  });
+$(".nav-toggle").on("click", () => {
+  $(".nav-toggle, .header-nav").toggleClass("show");
+});
+
+// 商品ニュース内の文字の表示
+$(".pickup-news li").on({
+  mouseenter: (e) => {
+    console.log(e);
+    $(e.target).children("p").fadeIn(100);
+  },
+  mouseout: (e) => {
+    $(e.target).children("p").fadeOut(100);
+  },
+});
+
+// ハンバーガーメニューのオンオフをactiveクラスの有無で判断する
+
+$(".navToggle").on("click", (e) => {
+  const target = $(e.currentTarget);
+  target.toggleClass("active");
+
+  if (target.hasClass("active")) {
+    $(".header-container nav").addClass("active");
+  } else {
+    $(".header-container nav").removeClass("active");
+  }
+});
+
+//sendボタン操作後、transmission completedボタンに変更
+
+$(".submit").on("click", () => {
+  $(".submit")
+    .animate({ width: "320px" }, 200)
+    .attr("value", "TRANSMISSION COMPLETED")
+    .addClass("active");
 });
